9f433f0887c8512ef7849fb7b7b3f3e6cc9f4063
Louis ARNAULT <l.arnault@kami.fr>
2025-08-06 15:15:50 +0200

    feat: add flask api endpoints (wip)

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+import json
+
+from flask import Flask
+from uuid import uuid4
+
 from src.blockchain import Blockchain
+
+# Instantiate the node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace("-", "")
+
+blockchain = Blockchain()
+
+@app.route("/mine", methods=["GET"])
+def mine():
+    return "We'll mine a new block"
+
+@app.route("/transactions/new", methods=["POST"])
+def new_transaction():
+    return "We'll add a new transaction"
+
+@app.route("/chain", methods=["GET"])
+def full_chain():
+    response = {
+        "chain": blockchain.chain,
+        "length": len(blockchain.chain),
+    }
+
+    return json.dumps(response).encode(), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
